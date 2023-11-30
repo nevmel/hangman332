5190580241bf1c9d833adc67128442d1944538a6
Nev Mellors <nevmellors@hotmail.com>
2023-11-30 14:55:02 +0000

    tidying up some style issues, irrelevant comments

--- milestone_3.py
+++ milestone_3.py
@@ -1,11 +1,10 @@
 def check_guess(guess):
-#word = "apple" #For testing purposes only
  word = set("apple")
  guess = guess.lower()
  if guess in word:
    print(f"Good guess, {guess} is in the word.")
  else:
-   print(f"Sorry, {guess} is not in the word. Try again.") #, ask_for_input()
+   print(f"Sorry, {guess} is not in the word. Try again.") 
 
 
 def ask_for_input():
@@ -19,11 +18,5 @@ def ask_for_input():
        
     
     check_guess(guess)   
-
-
-
-
-
-
 
 ask_for_input()
