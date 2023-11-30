e10452ed25d08e5cbadac0a27dd7b363385816ee
Nev Mellors <nevmellors@hotmail.com>
2023-11-30 12:22:14 +0000

    Bactracking on the code so it is in line with the mark scheme

--- milestone_2.py
+++ milestone_2.py
@@ -1,20 +1,23 @@
 import random
 
-def get_letter():
+word_list = ["apple","persimon","peach","raspberry","orange"]
+"""List for random choice of word"""
+
+word = random.choice(word_list)
+"""Choice randomiser"""
+
+"""def get_letter():
+    #Code to repeatedly ask for an input which has to be 1 character long and a string character (letter)
     guess = input("Please enter a single letter: ")
     if guess.isalpha()==True and len(guess)==1:
      return print("Good guess")
     else:
      return print("Oops! That is not a valid input. Please try again: "),get_letter()
+get_letter()"""
 
-
-
-word_list = ["apple","persimon","peach","raspberry","orange"]
-print(word_list)
-
-word = random.choice(word_list)
-print(word)
-
-
-get_letter()
+guess = input("Please enter a single letter: ")
+if guess.isalpha()==True and len(guess)==1:
+    print("Good guess")
+else:
+    print("Oops! That is not a valid input.")
 
