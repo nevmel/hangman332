bb36f1586c28f31b910bc34754b83fa51ad87d4b
Nev Mellors <nevmellors@hotmail.com>
2023-11-30 11:48:54 +0000

    Get a single letter from user

--- milestone_2.py
+++ milestone_2.py
@@ -1,15 +1,20 @@
 import random
 
+def get_letter():
+    guess = input("Please enter a single letter: ")
+    if guess.isalpha()==True and len(guess)==1:
+     return print("Good guess")
+    else:
+     return print("Oops! That is not a valid input. Please try again: "),get_letter()
+
+
+
 word_list = ["apple","persimon","peach","raspberry","orange"]
 print(word_list)
 
 word = random.choice(word_list)
 print(word)
 
-guess = input("Please enter a single letter: ")
 
-if guess.isalpha()==True and len(guess)==1:
-    print("Good guess")
-else:
-    print("Oops! That is not a valid input")
+get_letter()
 
