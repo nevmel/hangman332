7ac7c770ea7f655d4d68aef44466af42c9d3494b
Nev Mellors <nevmellors@hotmail.com>
2023-12-01 12:33:15 +0000

    Amended code up to check_gess for first iteration. Got some help on the call from Vander

--- milestone_4.py
+++ milestone_4.py
@@ -1,11 +1,13 @@
 import random
 
+
 class Hangman:
+    
     def __init__(self, word_list,num_lives = 5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = random.choice(word_list)
-        self.word_guessed = []
+        self.word = random.choice(self.word_list)
+        self.word_guessed = list('_' * len(self.word))
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
 
@@ -14,17 +16,27 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess, {guess} is in the word.")
+           # print(self.word_guessed)
+            for letters in range(len(self.word)):
+                if guess == self.word[letters]:
+                    self.word_guessed[letters] = guess
+            print(self.word_guessed)
+            self.num_letters -= 1
+            
 
     def ask_for_input(self):
-        while True:
+        
+        while  True:
             guess = input("Please choose a single letter: ")
-            if not guess.isalpha()==True and not len(guess)==1:
+            if guess.isalpha()!=True or len(guess)!=1:
                 print("Invalid letter. Please enter a single alphabetical character.")
-            elif guess in self.list_of_guesses:
+            elif guess in self.list_of_guesses: 
                 print("You already tried that letter!")
             else:
-                self.list_of_guesses.append(guess)
-                Hangman.check_guess(guess)
-                
-Hangman.ask_for_input("x")
+                 self.list_of_guesses.append(guess)
+                 self.check_guess(guess)
+                 break
+            
 
+hangman = Hangman(["apple","persimon","peach","raspberry","orange"])
+hangman.ask_for_input()
